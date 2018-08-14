b148dd874946580690c35207508c503266f303cd
tesujiro <tesujiro@gmail.com>
2018-08-15 03:48:41 +0800

    add: lib, len()

---
+++ lib/lib.go
@@ -0,0 +1,34 @@
+package lib
+
+import (
+	"reflect"
+
+	"github.com/tesujiro/goa/vm"
+)
+
+func Import(env *vm.Env) *vm.Env {
+	// length cannot return error -> TODO:change length to embedded func
+	length := func(val interface{}) int {
+		v := val.(reflect.Value)
+		switch v.Type().Kind() {
+		case reflect.String:
+			s := v.Interface().(string)
+			return len(s)
+		case reflect.Map:
+			s := v.Interface().(map[interface{}]interface{})
+			return len(s)
+		case reflect.Slice:
+			s := v.Interface().([]interface{})
+			return len(s)
+		default:
+			// fmt.Errorf("invalid argument %v (type %v) for len",s,reflect.TypeOf(s))
+			return 0
+		}
+	}
+	//env.Define("length", length)
+	env.Define("length", reflect.ValueOf(length))
+	//env.Define("len", length)
+	env.Define("len", reflect.ValueOf(length))
+
+	return env
+}
