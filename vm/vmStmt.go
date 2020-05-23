0df5e12f387c784591c612a38e7c948d01538ce5
tesujiro <tesujiro@gmail.com>
2020-05-24 05:13:29 +0900

    refact for ago-wasm

--- vm/vmStmt.go
+++ vm/vmStmt.go
@@ -195,52 +195,50 @@ func runSingleStmt(stmt ast.Stmt, env *Env) (interface{}, error) {
 		}
 		return nil, nil
 	case *ast.PrintStmt:
-		print := func(format string) (int, error) {
-			// make ast.CallExpr
-			callExpr := &ast.CallExpr{
-				Name:     "printf",
-				SubExprs: []ast.Expr{&ast.ParentExpr{SubExpr: &ast.StringExpr{Literal: format}}},
-			}
-			result, err := callFunc(callExpr, env)
-			if len(result.([]interface{})) == 0 {
-				return 0, err
-			}
-			return result.([]interface{})[0].(int), err
-		}
+		var line string
 		for i, expr := range stmt.Exprs {
 			result, err := evalExpr(expr, env)
 			if err != nil {
 				return nil, err
 			}
 			if 0 < i && i < len(stmt.Exprs) {
-				print(fmt.Sprintf("%v", env.builtin.OFS))
+				line += fmt.Sprintf("%v", env.builtin.OFS)
 			}
 			ofmt, _ := env.Get("OFMT")
 			switch reflect.ValueOf(result).Kind() {
 			case reflect.Float64:
-				print(fmt.Sprintf(ofmt.(string), result))
+				line += fmt.Sprintf(ofmt.(string), result)
 			case reflect.Int:
-				print(fmt.Sprintf("%v", result))
+				line += (fmt.Sprintf("%v", result))
 			case reflect.Int64:
-				print(fmt.Sprintf("%v", result))
+				line += (fmt.Sprintf("%v", result))
 			case reflect.String, reflect.Bool:
-				print(fmt.Sprintf("%v", result))
+				line += (fmt.Sprintf("%v", result))
 			case reflect.Slice:
 				len := reflect.ValueOf(result).Len()
 				for i := 0; i < len; i++ {
 					if i > 0 {
-						print(fmt.Sprintf("%v", env.builtin.OFS))
+						line += (fmt.Sprintf("%v", env.builtin.OFS))
 					}
-					print(fmt.Sprintf("%v", reflect.ValueOf(result).Index(i)))
+					line += (fmt.Sprintf("%v", reflect.ValueOf(result).Index(i)))
 				}
 			case reflect.Invalid:
-				print("")
 			default:
 				return nil, fmt.Errorf("type %s does not support print operation", reflect.ValueOf(result).Kind().String())
 			}
 		}
-		//fmt.Printf("%v", env.builtin.ORS)
-		print(fmt.Sprintf("%v", env.builtin.ORS))
+		line += env.builtin.ORS
+
+		// use printf func
+		callExpr := &ast.CallExpr{
+			Name:     "printf",
+			SubExprs: []ast.Expr{&ast.ParentExpr{SubExpr: &ast.StringExpr{Literal: line}}},
+		}
+		result, err := callFunc(callExpr, env)
+		if len(result.([]interface{})) == 0 {
+			return 0, err
+		}
+		return result.([]interface{})[0].(int), err
 	case *ast.IfStmt:
 		child := env.NewEnv()
 		result, err := evalExpr(stmt.If, child)
